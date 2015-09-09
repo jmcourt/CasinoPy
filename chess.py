d301ca5d3012d0bd8e4b8d6ea4f136a5c62e6d1c
jmcourt <J.M.Court@soton.ac.uk>
2015-09-09 19:10:00 +0100

    It can set up a board now!

--- chess.py
+++ chess.py
@@ -1,93 +1,122 @@
 #! /usr/bin/env python
 
 from numpy import array
+import platform
+import os
+
+#-----Create the Board----------------------------------------------------
 
 board=array([array([None]*8)]*8)
-print board
+turn=0
+turn_player=''
 
-def square_to_cell(square):                                               # Convery user input to grid location
+#-----Basic Function Definitions------------------------------------------
+
+def square_to_cell(square):                                               # Convert user input to grid location
    try:
       assert len(square)==2
       col,row=square[0],square[1]
       row=int(row)-1
-      if col=='a':
-         col=0
-      elif col=='b':
-         col=1
-      elif col=='c':
-         col=2
-      elif col=='d':
-         col=3
-      elif col=='e':
-         col=4
-      elif col=='f':
-         col=5
-      elif col=='g':
-         col=6
-      elif col=='h':
-         col=7
+
+      if col=='a':   col=0
+      elif col=='b': col=1
+      elif col=='c': col=2
+      elif col=='d': col=3
+      elif col=='e': col=4
+      elif col=='f': col=5
+      elif col=='g': col=6
+      elif col=='h': col=7
+
       assert row>=0
       assert row<=7
       assert col>=0
       assert col<=7
+
       return col,row
+
    except:
+
       return None,None
 
-#--------------------------------------------------------
+def clear():                                                              # Fetch operating system for the sake of system commands
+   system=platform.system().lower()
+   if system=='windows':
+      os.system('cls')
+   else:
+      os.system('clear')
+
+#-----Define Piece Superclass---------------------------------------------
 
 class piece:
    def __init__(self,square,colour,ID):
       self.col,self.row=square_to_cell(square)
       self.taken=False
       self.ID=ID
+      self.typ_val()
       self.colour=colour
+      self.last_moved=0
       board[self.col][self.row]=ID
 
+   def typ_val(self):
+      self.value=0
+      self.type=None
+
    def get_legal(self):
-      return [],[]
+      return []
 
    def move(self,square):
       board[self.col][self.row]=None
       self.col,self.row=square_to_cell(square)
-      board[self.col][self.row]=ID
+      if board[self.col][self.row]!=None:                                 # Take piece if on the square you're trying to move to
+          pieces[board[self.col][self.row]].take()
+      board[self.col][self.row]=self.ID
+      self.last_moved=turn
 
-   def take(self):
+   def take(self):                                                        # What to do if this piece is taken
       self.taken=True
       self.col=None
       self.row=None
+      score[self.colour]+=self.value
 
-   def try_move(self,square):
+   def try_move(self,square):                                             # Check if a move is allowed and, if so, do it.
       trycol,tryrow=square_to_cell(square)
-      if trycol in self.get_legal[0] and tryrow in self.get_legal[0]:
-         occupant=board[trycol,tryrow]
-         if occupant!=None:
-            pieces[occupant].take()
+      if (trycol,tryrow) in self.get_legal():
          self.move(square)
          return True
       else:
          return False
 
-
-      
+#-----Define Pieces-------------------------------------------------------
 
 class pawn(piece):
-   self.type='p'
+   def typ_val(self):
+      self.type='p'
+      self.value=1
 
 class knight(piece):
-   self.type='k'
+   def typ_val(self):
+      self.type='n'
+      self.value=3
 
 class bishop(piece):
-   self.type='b'
+   def typ_val(self):
+      self.type='b'
+      self.value=3
 
 class rook(piece):
-   self.type='r'
+   def typ_val(self):
+      self.type='r'
+      self.value=5
 
 class queen(piece):
-   self.type='q'
+   def typ_val(self):
+      self.type='q'
+      self.value=9
 
 class king(piece):
-   self.type=
+   def typ_val(self):
+      self.type='k'
+      self.value=0
 
 # King
 # Queen
@@ -96,7 +125,7 @@ class king(piece):
 # Bishop
 # Pawn
 
-
+#-----Setup the board-----------------------------------------------------
 
 pieces={ 'b_r1': rook('a1','b','b_r1'),
          'b_n1': knight('b1','b','b_n1'),
@@ -119,11 +148,63 @@ pieces={ 'b_r1': rook('a1','b','b_r1'),
 
 cols_ids=['a','b','c','d','e','f','g','h']
 
-for i in range(8):
+for i in range(8):                                                        # Do the pawns
    bpos=cols_ids[i]+'2'
    wpos=cols_ids[i]+'7'
    pieces['b_p'+str(i+1)]=pawn(bpos,'b','b_p'+str(i+1))
    pieces['w_p'+str(i+1)]=pawn(wpos,'w','w_p'+str(i+1))
 
-print board
+#-----Setup the stats-----------------------------------------------------
+
+score={ 'w' : 0, 'b' : 0 }
+
+#-----Setup the Screen----------------------------------------------------
+
+def header():
+
+   clear()
+   print '-------------------------'
+   print '-      Chess Game!      -'
+   print '-------------------------'
+   print ''
+   print 'Score: White-'+'{:2}'.format(str(score['b']))+'    Black-'+str(score['w'])
+   print ''
+   print 'Turn: '+(turn+'  ('+turn_player+'\'s move)' if turn>0 else '')
+   print ''
+
+   for j in range(8):
+      print '   +---+---+---+---+---+---+---+---+'
+      rstr='   |'
+      for i in range(8):
+         if board[i][j]==None:
+            rstr+='   '
+         else:
+            print_piece=pieces[board[i][j]].type
+            if pieces[board[i][j]].colour=='w':
+               print_piece=print_piece.upper()
+            rstr+=' '+print_piece+' '
+         rstr+='|'
+      print rstr
+   print '   +---+---+---+---+---+---+---+---+'
+   print ''
+   print ' -------------------------------------'
+   w_taken='     '
+   b_taken='     '
+   for ID in pieces:
+      piece=pieces[ID]
+      if piece.taken:
+         if piece.colour=='w':
+            w_taken=w_taken+piece.type.upper()+' '
+         else:
+            b_taken=b_taken+piece.type+' '
+   print w_taken
+   print ' -------------------------------------'
+   print b_taken
+   print ' -------------------------------------'
+   print ''
+
+header()
+
+
+
 
